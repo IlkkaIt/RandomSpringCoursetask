691d9df280ce778f6c0b31de8a56501c16ef61f5
IlkkaIt <ipylkko@gmail.com>
2021-12-20 10:10:16 +0200

    first commit

---
+++ src/main/java/filemanager/FileController.java
@@ -0,0 +1,62 @@
+package filemanager;
+ 
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+ 
+ 
+ 
+@Controller
+public class FileController {
+    
+   @Autowired
+   private fileObjectRepository filerepo;
+    
+ 
+ 
+ @GetMapping("/files")
+    public String list(Model model) {
+        model.addAttribute("files", this.filerepo.findAll(Sort.by(Sort.Direction.ASC, "name")));
+        return "files";
+    }
+    
+ 
+@PostMapping("/files")
+public String save(@RequestParam("file") MultipartFile file) throws IOException {
+    FileObject fo = new FileObject();
+ 
+    fo.setName(file.getOriginalFilename());
+    fo.setMediaType(file.getContentType());
+    fo.setSize(file.getSize());
+    fo.setContent(file.getBytes());
+ 
+    filerepo.save(fo);
+ 
+    return "redirect:/files";
+}
+ 
+@GetMapping("/files/{id}")
+public ResponseEntity<byte[]> viewFile(@PathVariable Long id) {
+    FileObject fo = filerepo.getOne(id);
+ 
+    final HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.parseMediaType(fo.getMediaType()));
+    headers.setContentLength(fo.getSize());
+    headers.add("Content-Disposition", "attachment; filename=" + fo.getName());
+ 
+    return new ResponseEntity<>(fo.getContent(), headers, HttpStatus.CREATED);
+}
+    
+    
+}
---
+++ src/main/java/filemanager/fileObjectRepository.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filemanager;
+ 
+import org.springframework.data.jpa.domain.AbstractPersistable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+ 
+@Repository
+public interface fileObjectRepository extends JpaRepository<FileObject, Long> {
+    
+    
+    
+}
