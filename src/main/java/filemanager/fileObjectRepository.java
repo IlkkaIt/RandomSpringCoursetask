d72f1b4c0e3f1c16140a37659ad59559b42f38d0
IlkkaIt <ipylkko@gmail.com>
2021-12-22 10:19:35 +0200

    netbeans commit

--- src/main/java/filemanager/fileObjectRepository.java
+++ src/main/java/filemanager/fileObjectRepository.java
@@ -5,7 +5,7 @@
  */
 package filemanager;
  
-import org.springframework.data.jpa.domain.AbstractPersistable;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
  
